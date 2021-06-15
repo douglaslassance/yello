4c03277aefe2f2b871d39fae5a86488bd9286f3f
Douglas Lassance <douglassance@gmail.com>
2021-06-15 12:04:08 -0700

    :sparkles: Add `ModeContext`

--- contexts.py
+++ contexts.py
@@ -1,29 +1,47 @@
 import copy
-import contextlib
 import bpy
-
-
-class SelectionContext:
-    """A context that will restore the original selection on exit."""
-
-    def __enter__(self, *args, **kwargs):
-        self.selected = [obj for obj in bpy.context.view_layer.objects.selected]
-        self.active = bpy.context.view_layer.objects.active
-        return self
-
-    def __exit__(self, *args, **kwargs):
-        bpy.ops.object.select_all(action="DESELECT")
-        for obj in self.selected:
-            obj.select_set(True)
-        bpy.context.view_layer.objects.active = self.active
 
 
 class CursorContext:
     """A context that will restore the original location of the cursor on exit."""
 
-    def __enter__(self, *args, **kwargs):
+    def __init__(self) -> None:
         self.location = copy.copy(bpy.context.scene.cursor.location)
+
+    def __enter__(self):
         return self
 
-    def __exit__(self, *args, **kwargs):
+    def __exit__(self, exc_type, exc_value, exc_traceback):
         bpy.context.scene.cursor.location = self.location
+
+
+class ModeContext:
+    """A context that will switch mode and revert to the previous mode on exit."""
+
+    def __init__(self, mode: str) -> None:
+        self._mode = mode
+        self.mode = bpy.context.mode
+
+    def __enter__(self):
+        bpy.ops.object.mode_set(mode=self._mode)
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        bpy.ops.object.mode_set(mode=self.mode.split("_")[0])
+
+
+class SelectionContext:
+    """A context that will restore the original selection on exit."""
+
+    def __init__(self) -> None:
+        self.selected = [obj for obj in bpy.context.view_layer.objects.selected]
+        self.active = bpy.context.view_layer.objects.active
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        bpy.ops.object.select_all(action="DESELECT")
+        for obj in self.selected:
+            obj.select_set(True)
+        bpy.context.view_layer.objects.active = self.active
