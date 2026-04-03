ee6e5af6aabc3f4c938fd0181f558c8ec98d1724
Douglas Lassance <hey@douglaslassance.me>
2026-04-03 16:31:09 -0700

    Move operators in correct module

---
+++ operators/file.py
@@ -0,0 +1,39 @@
+import os
+import subprocess
+import platform
+import bpy
+
+from .. import functions
+
+
+class MakeWritableOperator(bpy.types.Operator):
+    bl_idname = "object.make_writable"
+    bl_label = "Make Writable"
+    bl_description = "Perform a Gitarmony make writable on the current file"
+
+    @classmethod
+    def poll(cls, context):
+        return bool(bpy.data.filepath)
+
+    def execute(self, context):
+        functions.make_writable(bpy.data.filepath)
+        return {"FINISHED"}
+
+
+class OpenContainingFolderOperator(bpy.types.Operator):
+    bl_idname = "object.open_containing_folder"
+    bl_label = "Open Containing Folder"
+
+    @classmethod
+    def poll(cls, context):
+        return bool(bpy.data.filepath)
+
+    def execute(self, context):
+        filepath = bpy.data.filepath
+        if platform.system() == "Windows":
+            subprocess.Popen(["explorer", "/select,", filepath])
+        elif platform.system() == "Darwin":
+            subprocess.Popen(["open", "-R", filepath])
+        else:
+            subprocess.Popen(["xdg-open", os.path.dirname(filepath)])
+        return {"FINISHED"}
